6e4f326b58e5c65e19705691afea16827ec6dac2
Melltem <meltemqasimli@gmail.com>
2025-06-26 16:19:40 +0000

    a class Square that defines a square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 """This module defines a class Square that represents a square."""
+
+
 class Square:
     """Represents a square."""
+
+
     def __init__(self, size):
         self.__size = size
