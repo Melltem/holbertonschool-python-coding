6289619bbf6785b72dee1693baa6366e274ade55
Melltem <meltemqasimli@gmail.com>
2025-06-26 16:48:31 +0000

    property def size(self):

---
+++ python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This module defines a class Square that represents a square."""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    def size(self):
+        return self.__size
+    def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+~                                  
