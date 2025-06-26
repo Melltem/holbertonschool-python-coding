a240f015c48aa0759b561ee95a974ce945f4918b
Melltem <meltemqasimli@gmail.com>
2025-06-26 16:56:51 +0000

    property def size(self):

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -7,9 +7,11 @@ class Square:
 
     def __init__(self, size=0):
         self.__size = size
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
@@ -17,5 +19,6 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         return self.__size ** 2
